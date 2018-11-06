f2dec22a007197b98ec787c00840f1539c2ea367
xlxes <38281914+xlxes@users.noreply.github.com>
2018-11-06 23:23:22 +0800

    STM32 Hal库demo

---
+++ DEMO/HAL_DEMO/MDK-ARM/HAL_DEMO/dma.d
@@ -0,0 +1,29 @@
+hal_demo\dma.o: ../Src/dma.c
+hal_demo\dma.o: ../Inc/dma.h
+hal_demo\dma.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h
+hal_demo\dma.o: ../Inc/stm32f1xx_hal_conf.h
+hal_demo\dma.o: ../Inc/main.h
+hal_demo\dma.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h
+hal_demo\dma.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h
+hal_demo\dma.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h
+hal_demo\dma.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xe.h
+hal_demo\dma.o: ../Drivers/CMSIS/Include/core_cm3.h
+hal_demo\dma.o: D:\Program Files\keil\ARM\ARMCC\Bin\..\include\stdint.h
+hal_demo\dma.o: ../Drivers/CMSIS/Include/core_cmInstr.h
+hal_demo\dma.o: ../Drivers/CMSIS/Include/cmsis_armcc.h
+hal_demo\dma.o: ../Drivers/CMSIS/Include/core_cmFunc.h
+hal_demo\dma.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h
+hal_demo\dma.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h
+hal_demo\dma.o: D:\Program Files\keil\ARM\ARMCC\Bin\..\include\stdio.h
+hal_demo\dma.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h
+hal_demo\dma.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h
+hal_demo\dma.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h
+hal_demo\dma.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h
+hal_demo\dma.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h
+hal_demo\dma.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h
+hal_demo\dma.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h
+hal_demo\dma.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h
+hal_demo\dma.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h
+hal_demo\dma.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h
+hal_demo\dma.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h
+hal_demo\dma.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_uart.h
---
+++ DEMO/HAL_DEMO/MDK-ARM/HAL_DEMO/stm32f1xx_hal_gpio_ex.d
@@ -0,0 +1,28 @@
+hal_demo\stm32f1xx_hal_gpio_ex.o: ../Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c
+hal_demo\stm32f1xx_hal_gpio_ex.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h
+hal_demo\stm32f1xx_hal_gpio_ex.o: ../Inc/stm32f1xx_hal_conf.h
+hal_demo\stm32f1xx_hal_gpio_ex.o: ../Inc/main.h
+hal_demo\stm32f1xx_hal_gpio_ex.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h
+hal_demo\stm32f1xx_hal_gpio_ex.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h
+hal_demo\stm32f1xx_hal_gpio_ex.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h
+hal_demo\stm32f1xx_hal_gpio_ex.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xe.h
+hal_demo\stm32f1xx_hal_gpio_ex.o: ../Drivers/CMSIS/Include/core_cm3.h
+hal_demo\stm32f1xx_hal_gpio_ex.o: D:\Program Files\keil\ARM\ARMCC\Bin\..\include\stdint.h
+hal_demo\stm32f1xx_hal_gpio_ex.o: ../Drivers/CMSIS/Include/core_cmInstr.h
+hal_demo\stm32f1xx_hal_gpio_ex.o: ../Drivers/CMSIS/Include/cmsis_armcc.h
+hal_demo\stm32f1xx_hal_gpio_ex.o: ../Drivers/CMSIS/Include/core_cmFunc.h
+hal_demo\stm32f1xx_hal_gpio_ex.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h
+hal_demo\stm32f1xx_hal_gpio_ex.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h
+hal_demo\stm32f1xx_hal_gpio_ex.o: D:\Program Files\keil\ARM\ARMCC\Bin\..\include\stdio.h
+hal_demo\stm32f1xx_hal_gpio_ex.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h
+hal_demo\stm32f1xx_hal_gpio_ex.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h
+hal_demo\stm32f1xx_hal_gpio_ex.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h
+hal_demo\stm32f1xx_hal_gpio_ex.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h
+hal_demo\stm32f1xx_hal_gpio_ex.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h
+hal_demo\stm32f1xx_hal_gpio_ex.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h
+hal_demo\stm32f1xx_hal_gpio_ex.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h
+hal_demo\stm32f1xx_hal_gpio_ex.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h
+hal_demo\stm32f1xx_hal_gpio_ex.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h
+hal_demo\stm32f1xx_hal_gpio_ex.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h
+hal_demo\stm32f1xx_hal_gpio_ex.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h
+hal_demo\stm32f1xx_hal_gpio_ex.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_uart.h
